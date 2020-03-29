f18a916d71172325251537c228ea35e54d89d47c
Senatova93 <nanniin160@gmail.com>
2020-03-29 16:32:31 +0300

    Updates

---
+++ precache-manifest.ad3ccd94f66a0034758896706488a4c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea23ddf4ba55ff2c7c70745d5b67431b",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "db8738301fd819d7a0be",
+    "url": "/todo/static/css/main.9d1dd52d.chunk.css"
+  },
+  {
+    "revision": "6342dd87b01134c26f7c",
+    "url": "/todo/static/js/2.fa35d8d7.chunk.js"
+  },
+  {
+    "revision": "db8738301fd819d7a0be",
+    "url": "/todo/static/js/main.22a31383.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
